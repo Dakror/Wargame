77dcda8e3c881be42558ee266327641a07d45296
Dakror <mail@dakror.de>
2016-02-08 19:48:03 +0100

    screw steering behaviors

--- src/de/dakror/wargame/Wargame.java
+++ src/de/dakror/wargame/Wargame.java
@@ -169,7 +169,7 @@ public class Wargame extends ActivityStub {
 		
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				world.addEntity(new Unit(3, 3f, player, UnitType.Infantry));
+				world.addEntity(new Unit(5, 5f, player, UnitType.Infantry));
 			}
 		}
 		
